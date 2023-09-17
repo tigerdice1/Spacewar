26a87c74be4a252da55dd87fd284120d00e7fc2b
tigerdice1 <tigerdice1@gmail.com>
2023-09-18 04:17:50 +0900

    Update CameraController.cs

--- Spacewar/Assets/Spacewar/Scripts/CameraController.cs
+++ Spacewar/Assets/Spacewar/Scripts/CameraController.cs
@@ -16,9 +16,26 @@ public class CameraController : MonoBehaviour
     [Tooltip("카메라와 따라갈 물체 사이의 거리")]
     private Vector3 _offset;
 
+    private bool _isFollowingTarget;
+    
+    void SetFollowingState(bool isFollowingTarget){
+        _isFollowingTarget = isFollowingTarget;
+    }
+
+    void FollowTarget(){
+        if(_isFollowingTarget){
+            Vector3 fixedPosition = new Vector3(
+            _followObject.transform.position.x + _offset.x,
+            _followObject.transform.position.y + _offset.y,
+            _followObject.transform.position.z + _offset.z
+            );
+            _cameraObject.transform.position = Vector3.Lerp(_cameraObject.transform.position, fixedPosition, Time.deltaTime * 8);
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
+        SetFollowingState(true);
         Vector3 fixedPosition = new Vector3(
             _followObject.transform.position.x + _offset.x,
             _followObject.transform.position.y + _offset.y,
@@ -30,11 +47,6 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 fixedPosition = new Vector3(
-            _followObject.transform.position.x + _offset.x,
-            _followObject.transform.position.y + _offset.y,
-            _followObject.transform.position.z + _offset.z
-        );
-        _cameraObject.transform.position = Vector3.Lerp(_cameraObject.transform.position, fixedPosition, Time.deltaTime * 8);
+        FollowTarget();
     }
 }
