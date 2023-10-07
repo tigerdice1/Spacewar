5df73d7faafbbd184400ce3000d324e57f85afca
tigerdice1 <74129864+tigerdice1@users.noreply.github.com>
2023-10-07 15:58:53 +0900

    Update Junction.cs

--- Spacewar/Assets/Spacewar/Scripts/Junction.cs
+++ Spacewar/Assets/Spacewar/Scripts/Junction.cs
@@ -4,25 +4,31 @@ using UnityEngine;
 
 public class Junction : MonoBehaviour
 {
+	// 연결된 대상 발전기
     [SerializeField]
     private PowerGenerator _generator;
+	// 연결된 오브젝트 목록
     [SerializeField]
     private Electricity[] _connectedObjectsList;
-    private float _totalPower;
-    // Start is called before the first frame update
+ 	// 총 전력 소비량   
+	private float _totalPower;
+    
+
+	// 오브젝트 리스트의 파워 소비량을 불러와서 총 소비량을 계산함
     void UpdatePowerUsage(){
         _totalPower = 0.0f;
         foreach(Electricity connectedObject in _connectedObjectsList){
             _totalPower += connectedObject.PowerUsage; 
         }
     }
-
+	// 총 전력 소비량보다 파워 생산량이 적을 경우 파워에 로드율을 올리게끔 요청
     void SyncPowerUsage(){
         if(_generator.Power -_totalPower < 0.0f){
 
         }
     }
-    void Start()
+ 	// Start is called before the first frame update   
+	void Start()
     {
         
     }
