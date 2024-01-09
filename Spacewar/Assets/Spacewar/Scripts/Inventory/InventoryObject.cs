186c6476d9ee9aae296a0aa17dc22698e8d5ed84
TestSeung <sjlim589@hanmail.net>
2024-01-09 21:38:28 +0900

    Inventory Init.

---
+++ Spacewar/Assets/Spacewar/Scripts/Inventory/InventoryObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Inventory", menuName = "Inventory System/Inventory")]
+public class InventoryObject : ScriptableObject{
+   public List<InventorySlot> Container = new List<InventorySlot>();
+    //인벤토리에 항목 추가 함수
+    public void AddItem(ItemObject item, int amount){
+        bool hasItem = false;
+        for (int i = 0; i < Container.Count; i++){
+            if (Container[i]._item == item){
+                Container[i].AddAmount(amount);
+                hasItem = true;
+                break;
+            }
+        }
+        if (!hasItem){
+            Container.Add(new InventorySlot(item, amount));
+        }
+    }
+}
+
+[System.Serializable]
+public class InventorySlot{
+    public ItemObject _item;
+
+    //아이템의 양
+    public int _itemAmount;
+
+    //인벤토리슬롯이 생성될때 일부 값을 설정하는 생성자
+    public InventorySlot(ItemObject item, int amount){
+        _item = item;
+        _itemAmount = amount;
+    }
+    public void AddAmount(int value){
+        _itemAmount += value; 
+    }
+}
+
---
+++ Spacewar/Assets/Spacewar/Scripts/Item/ItemObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType{
+    Food,
+    Equipment,
+    Default
+}
+
+public abstract class ItemObject : ScriptableObject{ //항목을 생성하기 위한 기본 추상화 클래스
+    public GameObject _prefab; //인벤토리에 항목을 추가한 후 항목에 대한 디스플레이를 보관할 prefab 변수
+    public ItemType type;
+    [TextArea(15,20)]
+    public string _description; //  항목 설명을 담는 문자열
+}
