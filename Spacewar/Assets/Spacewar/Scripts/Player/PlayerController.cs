4b09f5a280732a481e5246e0efb66443ac542aa4
tigerdice1 <74129864+tigerdice1@users.noreply.github.com>
2024-02-11 07:18:56 +0900

    s

--- Spacewar/Assets/Spacewar/Scripts/Player/PlayerController.cs
+++ Spacewar/Assets/Spacewar/Scripts/Player/PlayerController.cs
@@ -50,14 +50,20 @@ public class PlayerController : MonoBehaviour
             _controlObject = _defaultControlObject;
         }
     }
-
-    private void LookCursor(){
+    private RaycastHit GetCursorRaycastResult(){
         Ray ray = gameObject.GetComponent<CameraController>().GetCamera().ScreenPointToRay(Input.mousePosition);
         RaycastHit hitResult;
         if(Physics.Raycast(ray, out hitResult)){
+            return hitResult;
+        }
+        else{
+            return hitResult;
+        }
+    }
+    private void LookCursor(){
+        RaycastHit hitResult = GetCursorRaycastResult();
             Vector3 mouseDir = new Vector3(hitResult.point.x, _controlObject.transform.position.y, hitResult.point.z) - _controlObject.transform.position;
             _controlObject.transform.forward = mouseDir;
-        }
         /*
         Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1f);
         Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
