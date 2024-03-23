34267f730f63507220b7a9abae28946e6aea2c53
tigerdice1 <tigerdice1@gmail.com>
2024-03-23 22:18:24 +0900

    d

--- Spacewar/Assets/Spacewar/Scripts/Electricity/Door.cs
+++ Spacewar/Assets/Spacewar/Scripts/Electricity/Door.cs
@@ -4,151 +4,48 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    // 단일개체
     [SerializeField]
-    [Tooltip("왼쪽 문")]
-    private Transform _leftDoor;
-    
-    [SerializeField]
-    [Tooltip("오른쪽 문")]
-    private Transform _rightDoor;
+    Transform _door;
+    Vector3 _doorClosedPosition;
 
-    private Transform[] _doors = new Transform[2];
-    private bool[] _isDoorLoaded = new bool[2];
-    private bool _isElectricityLoaded;
-    public Console_PowerGenerator _powerGenerator;
-
-    public float _doorOpenDistance; // 문이 열리는 거리
-    public float _doorOpenSpeed; // 문이 열리는 속도
-    public float _doorCloseTimer; // 플레이어가 떠난 후 문의 닫히기까지의 시간
-
-    private Vector3 _leftDoorClosedPosition; // 왼쪽 문의 닫힌 위치
-    private Vector3 _rightDoorClosedPosition; // 오른쪽 문의 닫힌 위치
-    private Vector3 _doorCurrentPosition; // 왼쪽 문의 현재 위치
-    private Vector3 _leftDoorOpenPosition; // 왼쪽 문의 열린 위치
-    private Vector3 _rightDoorOpenPosition; // 오른쪽 문의 열린 위치
-
-    private bool _isOpen = false; // 문이 열려있는지 여부
-    private float _autoCloseTimer; // 문이 자동으로 닫히는 시간을 카운트
-    private float _totalDistance; // 문이 열리는 총 거리
-    private Coroutine _doorCoroutine; // 문 이동 코루틴
-
+    float _autocloseTime = 2.0f;
+    float _timer;
+    bool _isTriggered;
     private void Initalize(){
-
+        _door = transform.Find("Corridor_Door");
+        _doorClosedPosition = _door.position;
     }
 
     private void Start()
     {
-        
-        //_powerGenerator = gameObject.GetComponent<PowerGenerator>();
-        _doorOpenDistance = 0.5f;
-        _doorOpenSpeed = 2f;
-        // 초기 위치 저장
-        _leftDoorClosedPosition = _leftDoor.localPosition;
-        _rightDoorClosedPosition = _rightDoor.localPosition;
+        Initalize();
 
-        // 열린 위치 계산
-        _leftDoorOpenPosition = _leftDoorClosedPosition + new Vector3(-1.0f, 0f,0f) * _doorOpenDistance;
-        _rightDoorOpenPosition = _rightDoorClosedPosition + new Vector3(-1.0f, 0f, 0f) * -_doorOpenDistance;
-
-        // 문이 열리는 길이를 계산
-        _totalDistance = Vector3.Distance(_leftDoorOpenPosition, _leftDoorClosedPosition);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (_powerGenerator.GetGeneratorState())
-        {
-            // 지금 문의 위치를 저장
-            _doorCurrentPosition = _leftDoor.localPosition;
-            // 자동문 타이머 0으로 설정
-            _autoCloseTimer = 0f;
-            if (_doorCoroutine != null)
-            {
-                StopCoroutine(_doorCoroutine);
-            }
-            if (!_isOpen && other.CompareTag("Player"))
-            {
-                _doorCoroutine = StartCoroutine(OpenDoors());
-            }
-        }
+     if(other.CompareTag("Player")){
+        _isTriggered = true;
+     }
     }
 
     private void OnTriggerStay(Collider other)
     {
-        _autoCloseTimer = 0f;
+        _timer = _autocloseTime;
     }
 
-    private IEnumerator OpenDoors()
-    {
-        //Debug.Log("Open");
-        if (_powerGenerator.GetGeneratorState())
-        {
-            //_electricity.IsActive = true;
-            float currentDistance = Vector3.Distance(_doorCurrentPosition, _leftDoorClosedPosition);
-            float elapsedTime = (currentDistance / _totalDistance) * _doorOpenSpeed;
-            while (elapsedTime < _doorOpenSpeed)
-            {
-                //if (_electricity.PowerUsage <= _electricity.PowerActive)
-                //{
-                //    _electricity.PowerUsage += Time.deltaTime * _doorOpenSpeed * 10;
-                //}
-                elapsedTime += Time.deltaTime;
-                float t = Mathf.Clamp01(elapsedTime / _doorOpenSpeed);
-                // Lerp 함수를 사용하여 문을 열기
-                _leftDoor.localPosition = Vector3.Lerp(_leftDoorClosedPosition, _leftDoorOpenPosition, t);
-                _rightDoor.localPosition = Vector3.Lerp(_rightDoorClosedPosition, _rightDoorOpenPosition, t);
-                yield return null;
-            }
-            _isOpen = true;
-            //_electricity.IsActive = false;
-            //_electricity.PowerUsage = _electricity.PowerIdle;
-        }
-
-    }
-
-    private IEnumerator CloseDoors()
-    {
-        if (_powerGenerator.GetGeneratorState())
-        {
-            //_electricity.IsActive = true;
-            float elapsedTime = 0f;
-            while (elapsedTime < _doorOpenSpeed)
-            {
-                //if (_electricity.PowerUsage <= _electricity.PowerActive)
-                //{
-                //    _electricity.PowerUsage += Time.deltaTime * _doorOpenSpeed * 10;
-                //}
-                elapsedTime += Time.deltaTime;
-                float t = Mathf.Clamp01(elapsedTime / _doorOpenSpeed);
-                // Lerp 함수를 사용하여 문을 닫기
-                _leftDoor.localPosition = Vector3.Lerp(_leftDoorOpenPosition, _leftDoorClosedPosition, t);
-                _rightDoor.localPosition = Vector3.Lerp(_rightDoorOpenPosition, _rightDoorClosedPosition, t);
-
-                yield return null;
-            }
-            //_electricity.IsActive = false;
-            //_electricity.PowerUsage = _electricity.PowerIdle;
-        }
+    private void OnTriggerExit(Collider other) {
+        _isTriggered = false;
     }
 
     private void Update()
     {
-        if (_powerGenerator.GetGeneratorState())
-        {
-            if (_isOpen)
-            {
-                _autoCloseTimer += Time.deltaTime;
-                if (_autoCloseTimer > _doorCloseTimer)
-                {
-                    _doorCoroutine = StartCoroutine(CloseDoors());
-                    _isOpen = false;
-                }
-            }
+        _timer += Time.deltaTime;
+        if(_isTriggered){
+            _door.position = Vector3.Lerp(_door.position, new Vector3(_door.position.x,-10.0f,_door.position.z), Time.deltaTime);
         }
-        else if (!_powerGenerator.GetGeneratorState())
-        {
-            //_electricity.PowerUsage = 0f;
+        else if(!_isTriggered && _timer >= _autocloseTime){
+            _door.position = Vector3.Lerp(_door.position, _doorClosedPosition, Time.deltaTime);
         }
     }
 }
