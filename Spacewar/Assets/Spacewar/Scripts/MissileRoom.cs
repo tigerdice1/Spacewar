0a3fdf7eb0bac1336a3988d5b334f1a1dc01ead2
tigerdice1 <tigerdice1@gmail.com>
2024-03-23 23:32:23 +0900

    d

---
+++ Spacewar/Assets/Spacewar/Scripts/MissileRoom.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileRoom : MonoBehaviour
+{
+    [SerializeField]
+    float _doorTimer;
+    [SerializeField]
+    bool _missileLoadSeq;
+    [SerializeField]
+    bool _isMissileLoaded;
+    [SerializeField]
+    Transform _innerDoorLeft;
+    [SerializeField]
+    Transform _innerDoorRight;
+    [SerializeField]
+    Transform _outerDoorLeft;
+    [SerializeField]
+    Transform _outerDoorRight;
+    [SerializeField]
+    Vector3 _innerDoorLeftClosedPosition;
+    [SerializeField]
+    Vector3 _innerDoorRightClosedPosition;
+    [SerializeField]
+    Vector3 _outerDoorLeftClosedPosition;
+    [SerializeField]
+    Vector3 _outerDoorRightClosedPosition;
+    [SerializeField]
+    Vector3 _innerDoorLeftOpenPosition;
+    [SerializeField]
+    Vector3 _innerDoorRightOpenPosition;
+    [SerializeField]
+    Vector3 _outerDoorLeftOpenPosition;
+    [SerializeField]
+    Vector3 _outerDoorRightOpenPosition;
+
+    void Initalize(){
+        _innerDoorLeft = transform.Find("Room_Missile_InnerDoorLeft");
+        _innerDoorRight = transform.Find("Room_Missile_InnerDoorRight");
+        _outerDoorLeft = transform.Find("Room_Missile_OuterDoorLeft");
+        _outerDoorRight = transform.Find("Room_Missile_OuterDoorRight");
+        _innerDoorLeftClosedPosition= _innerDoorLeft.position;
+        _innerDoorRightClosedPosition = _innerDoorRight.position;
+        _outerDoorLeftClosedPosition = _outerDoorLeft.position;
+        _outerDoorRightClosedPosition = _outerDoorRight.position;
+
+        _innerDoorLeftOpenPosition = new Vector3(_innerDoorLeftClosedPosition.x - 3.5f, _innerDoorLeftClosedPosition.y, _innerDoorLeftClosedPosition.z);
+        _innerDoorRightOpenPosition = new Vector3(_innerDoorRightClosedPosition.x + 3.5f, _innerDoorRightClosedPosition.y, _innerDoorRightClosedPosition.z);
+        _innerDoorLeftOpenPosition = new Vector3(_innerDoorLeftClosedPosition.x - 3.5f, _innerDoorLeftClosedPosition.y, _innerDoorLeftClosedPosition.z);
+        _innerDoorRightOpenPosition = new Vector3(_innerDoorRightClosedPosition.x + 3.5f, _innerDoorRightClosedPosition.y, _innerDoorRightClosedPosition.z);
+        _outerDoorLeftOpenPosition = new Vector3(_outerDoorLeftClosedPosition.x - 3.5f, _outerDoorLeftClosedPosition.y, _outerDoorLeftClosedPosition.z);
+        _outerDoorRightOpenPosition = new Vector3(_outerDoorRightClosedPosition.x + 3.5f, _outerDoorRightClosedPosition.y, _outerDoorRightClosedPosition.z);
+        _outerDoorLeftOpenPosition = new Vector3(_outerDoorLeftClosedPosition.x - 3.5f, _outerDoorLeftClosedPosition.y, _outerDoorLeftClosedPosition.z);
+        _outerDoorRightOpenPosition = new Vector3(_outerDoorRightClosedPosition.x + 3.5f, _outerDoorRightClosedPosition.y, _outerDoorRightClosedPosition.z);
+        if(!_isMissileLoaded){
+            _innerDoorLeft.position = _innerDoorLeftOpenPosition;
+            _innerDoorRight.position = _innerDoorRightOpenPosition;
+            _missileLoadSeq = false;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Initalize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_missileLoadSeq && !_isMissileLoaded){
+            _doorTimer += Time.deltaTime;
+            _outerDoorLeft.position = Vector3.Lerp(_outerDoorLeft.position, _outerDoorLeftClosedPosition, Time.deltaTime);
+            _outerDoorRight.position = Vector3.Lerp(_outerDoorRight.position, _outerDoorRightClosedPosition, Time.deltaTime);
+            if(_doorTimer >= 5f){
+                _innerDoorLeft.position = Vector3.Lerp(_innerDoorLeft.position, _innerDoorLeftOpenPosition, Time.deltaTime);
+                _innerDoorRight.position = Vector3.Lerp(_innerDoorRight.position, _innerDoorRightOpenPosition, Time.deltaTime);
+            }
+            if(_doorTimer >= 10){
+                _doorTimer = 0f;
+                _missileLoadSeq = false;
+            }
+        }
+        if(_missileLoadSeq){
+            _doorTimer += Time.deltaTime;
+            _innerDoorLeft.position = Vector3.Lerp(_innerDoorLeft.position, _innerDoorLeftClosedPosition, Time.deltaTime);
+            _innerDoorRight.position = Vector3.Lerp(_innerDoorRight.position, _innerDoorRightClosedPosition, Time.deltaTime);
+            if(_doorTimer >= 5f){
+                _outerDoorLeft.position = Vector3.Lerp(_outerDoorLeft.position, _outerDoorLeftOpenPosition, Time.deltaTime);
+                _outerDoorRight.position = Vector3.Lerp(_outerDoorRight.position, _outerDoorRightOpenPosition, Time.deltaTime);
+            }
+            if(_doorTimer >= 10){
+                _isMissileLoaded = true;
+                _doorTimer = 0f;
+                _missileLoadSeq = false;
+            }
+        }
+    }
+}
