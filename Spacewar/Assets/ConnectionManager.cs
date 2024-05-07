d7e9e0b5f369eaa3f4e485a93f364ee46474ddb8
tigerdice1 <tigerdice1@gmail.com>
2024-05-08 01:46:00 +0900

    Update ConnectionManager.cs

--- Spacewar/Assets/ConnectionManager.cs
+++ Spacewar/Assets/ConnectionManager.cs
@@ -1,9 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
 
-public class ConnectionManager : MonoBehaviour
+public class ConnectionManager : MonoBehaviourPunCallbacks
 {
+    [SerializeField]
+    private Button _connectButton;
+    private Text _connectionStatus;
+
+    public override void OnConnectedToMaster(){
+        print("서버 접속 완료");
+        PhotonNetwork.LocalPlayer.NickName = "sdd";
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby(){
+        print("로비 접속 완료");
+    }
+
+    public void Connect() => PhotonNetwork.ConnectUsingSettings();
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +28,9 @@ public class ConnectionManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
+        //_connectionStatus.text = PhotonNetwork.NetworkClientState.ToString();
+        
         
     }
 }
